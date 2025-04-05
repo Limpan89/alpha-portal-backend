fec0e4214eaefa147f80011d119ae1cac782505c
Limpan89 <lbroback89@gmail.com>
2025-04-06 01:20:39 +0200

    Initial commit

---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Data.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<bool> AddAsync(TEntity entity);
+    Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+    Task<IEnumerable<TEntity>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? filterBy = null, params Expression<Func<TEntity, object>>[] includes);
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes);
+    Task<bool> UpdateAsync(TEntity entity);
+}
---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,113 @@
+﻿using Data.Contexts;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+
+public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    protected readonly DataContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    protected BaseRepository(DataContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        return await _dbSet.AnyAsync(expression);
+    }
+
+    public virtual async Task<bool> AddAsync(TEntity entity)
+    {
+        if (entity == null)
+            return false;
+
+        try
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+            return await _dbSet.ContainsAsync(entity);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public virtual async Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        var entity = await _dbSet.FirstOrDefaultAsync(expression);
+        if (entity == default)
+            return false;
+
+        try
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return await _dbSet.AnyAsync(expression);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null,
+        Expression<Func<TEntity, bool>>? filterBy = null, params Expression<Func<TEntity, object>>[] includes)
+    {
+        IQueryable<TEntity> query = _dbSet;
+
+        if (filterBy != null)
+            query = query.Where(filterBy);
+
+        if (includes != null && includes.Length > 0)
+        {
+            foreach (var i in includes)
+                query = query.Include(i);
+        }
+
+        if (sortBy != null)
+            query = orderByDescending ? query.OrderByDescending(sortBy) : query.OrderBy(sortBy);
+
+        return await query.ToListAsync();
+    }
+
+    public virtual async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes)
+    {
+        IQueryable<TEntity> query = _dbSet;
+
+        if (includes != null && includes.Length > 0)
+        {
+            foreach (var i in includes)
+                query = query.Include(i);
+        }
+
+        var entity = await query.FirstOrDefaultAsync(findBy);
+        return entity ?? null;
+    }
+
+    public virtual async Task<bool> UpdateAsync(TEntity entity)
+    {
+        if (entity == null)
+            return false;
+
+        try
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return _dbSet.Contains(entity);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+}
