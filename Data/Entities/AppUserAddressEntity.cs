33a35eec468297c4fed1f7568df9776e4be883da
Limpan89 <lbroback89@gmail.com>
2025-04-06 12:13:41 +0200

    Initial commit

---
+++ Data/Entities/AppUserAddressEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class AppUserAddressEntity
+{
+    [Key, ForeignKey(nameof(User))]
+    public string UserId { get; set; }
+    public AppUserEntity User { get; set; } = null!;
+
+    public string StreetName { get; set; } = null!;
+    public string? StreetNumber { get; set; }
+
+    [ForeignKey(nameof(PostalCodeId))]
+    public string PostalCodeId { get; set; } = null!;
+    public PostalCodeEntity PostalCode { get; set; }
+}
---
+++ Data/Entities/AppUserEntity.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Data.Entities;
+
+public class AppUserEntity : IdentityUser
+{
+    public AppUserProfileEntity? Profile { get; set; }
+    public AppUserAddressEntity? Address { get; set; }
+}
