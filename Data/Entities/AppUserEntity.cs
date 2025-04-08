21b4bb0c4f54b83cdefd8e2749de42dfeacf3638
Limpan89 <lbroback89@gmail.com>
2025-04-08 21:07:21 +0200

    Removed

--- Data/Entities/AppUserEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Data.Entities;
-
-public class AppUserEntity : IdentityUser
-{
-    public AppUserProfileEntity? Profile { get; set; }
-    public AppUserAddressEntity? Address { get; set; }
-}
