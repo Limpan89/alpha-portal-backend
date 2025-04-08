2efcdcbb0c019c784411e64e1a6b81aa351bff4d
Limpan89 <lbroback89@gmail.com>
2025-04-08 21:08:18 +0200

    Initial commit

---
+++ Data/Entities/UserProfileEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+public class UserProfileEntity
+{
+    [Key, ForeignKey(nameof(User))]
+    public string UserId { get; set; } = null!;
+    public UserEntity User { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Phone { get; set; }
+    public string? Image { get; set; }
+}
