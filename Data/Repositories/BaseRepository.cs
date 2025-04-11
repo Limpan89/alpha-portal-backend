7e003a07b4363c8c0ff17a5b5b3332c107e85306
Limpan89 <lbroback89@gmail.com>
2025-04-11 22:55:17 +0200

    implemented caching

--- Data/Repositories/BaseRepository.cs
+++ Data/Repositories/BaseRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Data.Contexts;
 using Domain.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 using System.Linq.Expressions;
@@ -23,14 +22,14 @@ public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity,
     protected readonly DataContext _context;
     protected readonly DbSet<TEntity> _dbSet;
     protected readonly IMemoryCache _cache;
-    protected string _cacheKey_All;
+    protected readonly List<string> _cachedKeys;
 
     protected BaseRepository(DataContext context, IMemoryCache cache)
     {
         _context = context;
         _dbSet = _context.Set<TEntity>();
         _cache = cache;
-        _cacheKey_All = $"${typeof(TEntity).Name}_All";
+        _cachedKeys = new List<string>();
     }
 
     public virtual async Task<RepositoryResult> ExistsAsync(Expression<Func<TEntity, bool>> findBy)
@@ -51,7 +50,27 @@ public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity,
         {
             _dbSet.Add(entity);
             await _context.SaveChangesAsync();
+            ClearCache();
             return new RepositoryResult { Succeeded = true, StatusCode = 201 };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new RepositoryResult { Succeeded = false, StatusCode = 500, Message = ex.Message };
+        }
+    }
+
+    public virtual async Task<RepositoryResult> UpdateAsync(TEntity entity)
+    {
+        if (entity == null)
+            return new RepositoryResult { Succeeded = false, StatusCode = 400 };
+
+        try
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            ClearCache();
+            return new RepositoryResult { Succeeded = true, StatusCode = 200 };
         }
         catch (Exception ex)
         {
@@ -70,6 +89,7 @@ public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity,
         {
             _dbSet.Remove(entity);
             await _context.SaveChangesAsync();
+            ClearCache();
             return new RepositoryResult { Succeeded = true, StatusCode = 200 };
         }
         catch (Exception ex)
@@ -82,6 +102,11 @@ public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity,
     public virtual async Task<RepositoryResult<IEnumerable<TModel>>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null,
         Expression<Func<TEntity, bool>>? filterBy = null, params Expression<Func<TEntity, object>>[] includes)
     {
+        string cacheKey = GenerateCacheKey(orderByDescending, sortBy, filterBy, includes);
+        IEnumerable<TModel>? models;
+        if (_cache.TryGetValue(cacheKey, out models))
+            return new RepositoryResult<IEnumerable<TModel>> { Succeeded = true, StatusCode = 200, Result = models };
+
         IQueryable<TEntity> query = _dbSet;
 
         if (filterBy != null)
@@ -97,12 +122,19 @@ public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity,
             query = orderByDescending ? query.OrderByDescending(sortBy) : query.OrderBy(sortBy);
 
         var entities = await query.ToListAsync();
-        var models = entities.Select(e => MappingExtensions.MapTo<TModel>(e));
+        models = entities.Select(e => MappingExtensions.MapTo<TModel>(e));
+        AddToCache<IEnumerable<TModel>>(cacheKey, models);
+
         return new RepositoryResult<IEnumerable<TModel>> { Succeeded = true, StatusCode = 200, Result = models };
     }
 
     public virtual async Task<RepositoryResult<TModel>> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes)
     {
+        TModel? model;
+        string cacheKey = GenerateCacheKey(findBy, includes);
+        if (_cache.TryGetValue<TModel>(cacheKey, out model))
+            return new RepositoryResult<TModel> { Succeeded = true, StatusCode = 200, Result = model };
+
         IQueryable<TEntity> query = _dbSet;
 
         if (includes != null && includes.Length > 0)
@@ -115,25 +147,41 @@ public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity,
         if (entity == null)
             return new RepositoryResult<TModel> { Succeeded = false, StatusCode = 404 };
 
-        var model = MappingExtensions.MapTo<TModel>(entity);
-        return new RepositoryResult<TModel> { Succeeded = true, StatusCode = 200, Result = model };
+        model = MappingExtensions.MapTo<TModel>(entity);
+        AddToCache<TModel>(cacheKey, model);
+
+        return new RepositoryResult<TModel> { Succeeded = true, StatusCode = 200, Result = model! };
     }
 
-    public virtual async Task<RepositoryResult> UpdateAsync(TEntity entity)
+    #region "Cache"
+    public string GenerateCacheKey(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes)
     {
-        if (entity == null)
-            return new RepositoryResult { Succeeded = false, StatusCode = 400 };
-
-        try
-        {
-            _dbSet.Update(entity);
-            await _context.SaveChangesAsync();
-            return new RepositoryResult { Succeeded = true, StatusCode = 200 };
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-            return new RepositoryResult { Succeeded = false, StatusCode = 500, Message = ex.Message };
-        }
+        string includeFragment = (includes != null && includes.Length != 0 ? string.Join("_", includes.Select(i => i.ToString())) : "");
+        return $"{typeof(TEntity).Name}_{findBy.ToString()}_{includeFragment}";
     }
+
+    public string GenerateCacheKey(bool orderByDescending, Expression<Func<TEntity, object>>? sortBy, Expression<Func<TEntity, bool>>? filterBy, params Expression<Func<TEntity, object>>[] includes)
+    {
+        string orderFragment = orderByDescending ? "_descending" : "_ascending";
+        string sortFragment = sortBy != null ? $"_{sortBy.ToString()}" : "";
+        string filterFragment = filterBy != null ? $"_{filterBy.ToString()}" : "";
+        string includeFragment = includes != null && includes.Length != 0 ? string.Join("_", includes.Select(i => i.ToString())) : "";
+        return $"{typeof(TEntity).Name}_All{orderFragment}{sortFragment}{filterFragment}_{includeFragment}";
+
+    }
+
+    public void ClearCache()
+    {
+        foreach (var key in _cachedKeys)
+            _cache.Remove(key);
+        _cachedKeys.Clear();
+    }
+
+    public void AddToCache<T>(string key, T value)
+    {
+        _cache.CreateEntry(key);
+        _cache.Set<T>(key, value);
+        _cachedKeys.Add(key);
+    }
+    #endregion
 }
