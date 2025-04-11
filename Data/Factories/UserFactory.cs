aa492a15d0d5977ad1ad2064afab68b09dd20fc3
Limpan89 <lbroback89@gmail.com>
2025-04-11 22:54:05 +0200

    added map func

--- Data/Factories/UserFactory.cs
+++ Data/Factories/UserFactory.cs
@@ -1,11 +1,12 @@
 ﻿using Data.Entities;
 using Domain.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Data.Factories;
 
 public static class UserFactory
 {
-    public static UserModel Create(UserEntity entity)
+    public static UserModel MapEntityToModel(UserEntity entity)
     {
         var model = new UserModel
         {
@@ -22,5 +23,33 @@ public static class UserFactory
             PostalCode = entity.Address?.PostalCode
         };
         return model;
+    }
+
+    public static UserEntity MapModelToEntity(UserModel model)
+    {
+        var entity = new UserEntity
+        {
+            Id = model.Id,
+            Email = model.Email,
+            UserName = model.Email,
+
+            Profile = new UserProfileEntity
+            {
+                UserId = model.Id,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Phone = model.Phone,
+                Image = model.Image
+            },
+
+            Address = new UserAddressEntity
+            {
+                UserId = model.Id,
+                StreetAddress = model.StreetAddress,
+                CityName = model.CityName,
+                PostalCode = model.PostalCode
+            }
+        };
+        return entity;
     }
 }
