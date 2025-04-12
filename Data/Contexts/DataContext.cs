b2cca07291d67e74f3944a2f68c2947a907876df
Limpan89 <lbroback89@gmail.com>
2025-04-12 14:31:28 +0200

    Added entities to context

--- Data/Contexts/DataContext.cs
+++ Data/Contexts/DataContext.cs
@@ -8,4 +8,7 @@ public class DataContext(DbContextOptions<DataContext> options) : IdentityDbCont
 {
     public DbSet<UserProfileEntity> UserProfiles { get; set; }
     public DbSet<UserAddressEntity> UserAddresses { get; set; }
+    public DbSet<ProjectEntity> Projects { get; set; }
+    public DbSet<StatusEntity> Status { get; set; }
+    public DbSet<ClientEntity> Clients { get; set; }
 }
