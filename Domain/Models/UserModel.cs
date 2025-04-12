ddd7c38267a039caaa882b392ba5707de0243551
Limpan89 <lbroback89@gmail.com>
2025-04-12 13:41:02 +0200

    Added jobtitle & role

--- Domain/Models/UserModel.cs
+++ Domain/Models/UserModel.cs
@@ -7,8 +7,10 @@ public class UserModel
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Phone { get; set; }
+    public string? JobTitle { get; set; }
     public string? Image { get; set; }
     public string? StreetAddress { get; set; }
     public string? CityName { get; set; }
     public string? PostalCode { get; set; }
+    public string? Role { get; set; } = null;
 }
