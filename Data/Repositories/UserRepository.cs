873086a9313fc253a23a45a4092c24267ee06afa
Limpan89 <lbroback89@gmail.com>
2025-04-13 22:44:41 +0200

    fully supports usermanager for crud operations

--- Data/Repositories/UserRepository.cs
+++ Data/Repositories/UserRepository.cs
@@ -1,9 +1,12 @@
 ﻿using Data.Contexts;
 using Data.Entities;
+using Data.Factories;
 using Domain.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
+using System.Linq.Expressions;
 
 namespace Data.Repositories;
 
@@ -12,7 +15,7 @@ public interface IUserRepository : IBaseRepository<UserEntity, UserModel>
     public Task<RepositoryResult> AddAsync(UserEntity entity, string password);
 }
 
-public class UserRepository(DataContext context, IMemoryCache cache, UserManager<UserEntity> userManager) : BaseRepository<UserEntity, UserModel>(context, cache), IUserRepository
+public class UserRepository(DataContext context, IMemoryCache cache, UserManager<UserEntity> userManager) : BaseRepository<UserEntity, UserModel>(context, cache, new UserModelFactory()), IUserRepository
 {
     private readonly UserManager<UserEntity> _userManager = userManager;
 
@@ -23,7 +26,28 @@ public class UserRepository(DataContext context, IMemoryCache cache, UserManager
 
         try
         {
-            int users = _userManager.Users.Count();
+            var result = await _userManager.CreateAsync(entity, password);
+
+            if (!result.Succeeded)
+                throw new Exception("Failed to create User.");
+
+            ClearCache();
+            return new RepositoryResult { Succeeded = true, StatusCode = 201 };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new RepositoryResult { Succeeded = false, StatusCode = 500, Message = ex.Message };
+        }
+    }
+
+    public override async Task<RepositoryResult> AddAsync(UserEntity entity)
+    {
+        if (entity == null)
+            return new RepositoryResult { Succeeded = false, StatusCode = 400 };
+
+        try
+        {
             var result = await _userManager.CreateAsync(entity);
 
             if (!result.Succeeded)
@@ -31,6 +55,51 @@ public class UserRepository(DataContext context, IMemoryCache cache, UserManager
 
             ClearCache();
             return new RepositoryResult { Succeeded = true, StatusCode = 201 };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new RepositoryResult { Succeeded = false, StatusCode = 500, Message = ex.Message };
+        }
+    }
+
+    public override async Task<RepositoryResult> UpdateAsync(UserEntity entity)
+    {
+        if (entity == null)
+            return new RepositoryResult { Succeeded = false, StatusCode = 400 };
+
+        try
+        {
+            var result = await _userManager.UpdateAsync(entity);
+
+            if (!result.Succeeded)
+                throw new Exception("Failed to update User.");
+
+            ClearCache();
+            return new RepositoryResult { Succeeded = true, StatusCode = 200 };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new RepositoryResult { Succeeded = false, StatusCode = 500, Message = ex.Message };
+        }
+    }
+
+    public override async Task<RepositoryResult> DeleteAsync(Expression<Func<UserEntity, bool>> expression)
+    {
+        var entity = await _dbSet.FirstOrDefaultAsync(expression);
+        if (entity == default)
+            return new RepositoryResult { Succeeded = false, StatusCode = 400 };
+
+        try
+        {
+            var result = await _userManager.DeleteAsync(entity);
+
+            if (!result.Succeeded)
+                throw new Exception("Failed to remove User.");
+
+            ClearCache();
+            return new RepositoryResult { Succeeded = true, StatusCode = 200 };
         }
         catch (Exception ex)
         {
