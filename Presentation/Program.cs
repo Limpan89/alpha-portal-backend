897956bc6813df09450f6c22e99f631e7331d39e
Limpan89 <lbroback89@gmail.com>
2025-04-13 22:39:30 +0200

    added services

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,6 +1,8 @@
 
+using Business.Services;
 using Data.Contexts;
 using Data.Entities;
+using Data.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,6 +20,17 @@ public class Program
         builder.Services.AddDbContext<DataContext>(e => e.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
         builder.Services.AddIdentity<UserEntity, IdentityRole>().AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
 
+        builder.Services.AddScoped<IClientRepository, ClientRepository>();
+        builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+        builder.Services.AddScoped<IStatusRepository, StatusRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IPostalAddressRepository, PostalAddressRepository>();
+
+        builder.Services.AddScoped<IClientService, ClientService>();
+        builder.Services.AddScoped<IProjectService, ProjectService>();
+        builder.Services.AddScoped<IStatusService, StatusService>();
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<IPostalAddressService, PostalAddressService>();
 
         var app = builder.Build();
         app.MapOpenApi();
