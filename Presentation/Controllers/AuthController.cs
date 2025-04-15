54ca4f81f1196fac8979609948d471a57a974b90
Limpan89 <lbroback89@gmail.com>
2025-04-15 22:15:10 +0200

    login signin

--- Presentation/Controllers/AuthController.cs
+++ Presentation/Controllers/AuthController.cs
@@ -1,15 +1,43 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Models;
+using Business.Services;
+using Domain.Extensions;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IAuthService authService) : ControllerBase
     {
-        public IActionResult SignUp()
+        private readonly IAuthService _authService = authService;
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> SignIn(SignInFormViewModel form)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (ModelState.IsValid)
+            {
+                var result = await _authService.SignInAsync(form.MapTo<SignInFormDto>());
+                if (result.Succeeded)
+                    return Ok(new { token = result.Result });
+            }
+            return Unauthorized(new { error = "Invalid email or password." });
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SingUp(SignUpFormViewModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _authService.SignUpAsync(form.MapTo<SignUpFormDto>());
+                if (result.Succeeded)
+                    return Ok(new { message = "User Created" });
+                return Unauthorized(new { error = "Invalid signup form.", status = result.StatusCode });
+            }
+            return Unauthorized(new { error = "Invalid signup form." });
         }
     }
 }
