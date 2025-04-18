c1d40bf1f73cca287585911d1ec9e9f63c79bece
Limpan89 <lbroback89@gmail.com>
2025-04-18 21:20:19 +0200

    improved error handling

--- Business/Handlers/JwtTokenHandler.cs
+++ Business/Handlers/JwtTokenHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -18,28 +19,45 @@ public class JwtTokenHandler
 
     public string GenerateToken(UserModel user, string? role = null)
     {
-        var key = Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]!);
-        var issuer = _configuration["Jwt:Issuer"];
+        try
+        {
 
-        var claims = new List<Claim>
+
+            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]!);
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+
+            var claims = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, user.Id),
-            //new(ClaimTypes.Email, user.Email)
+            new(ClaimTypes.Email, user.Email)
         };
 
-        if (role != null)
-            claims.Add(new(ClaimTypes.Role, role));
+            if (!string.IsNullOrEmpty(role))
+            {
+                claims.Add(new(ClaimTypes.Role, role));
 
-        var tokenDescriptor = new SecurityTokenDescriptor
+                if (role == "Admin")
+                    claims.Add(new("apiKey", _configuration["SecretKeys:Admin"]!));
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = issuer,
+                Audience = audience,
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+        catch (Exception ex)
         {
-            Issuer = issuer,
-            Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddHours(1),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-        };
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        return tokenHandler.WriteToken(token);
+            Debug.WriteLine(ex.Message);
+            return null;
+        }
     }
 }
