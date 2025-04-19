5b3f2a50d3a909322166a243aca5ba0e6a4c184f
Limpan89 <lbroback89@gmail.com>
2025-04-19 10:46:33 +0200

    Initial commit

---
+++ Business/Models/AuthResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Business.Models;
+
+public class AuthResult
+{
+    public bool Succeeded { get; set; }
+    public int StatusCode { get; set; }
+    public string? Token { get; set; }
+    public bool IsAdmin { get; set; }
+    public string? ApiKey { get; set; }
+    public string? UserId { get; set; }
+}
